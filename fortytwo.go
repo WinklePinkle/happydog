bee1d40ecd600979977896de0b7290a563b36e3e
WinklePinkle <comclaug@gmail.com>
2020-10-28 09:44:12 -0700

    short declarations

---
+++ fortytwo.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	)
+
+func main(){
+	x := 42
+	// declare and assign value
+	fmt.Println("What is the answer to the ultimate question of life, the universe and everything?")
+	fmt.Println(x)
+	x = 99
+	// assign new value to existing declaration
+	y := 100 + 24
+	fmt.Println(y)
+}
